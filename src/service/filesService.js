7e9dd09a0ba90721dc4bbab2fd95a9c300b4cbdb
markCornejo <t_falcom@hotmail.com>
2021-06-21 17:40:31 -0500

    [Add]  correccion

--- src/service/filesService.js
+++ src/service/filesService.js
@@ -1,4 +1,5 @@
 const help = require('../helpers/help');
+const path = require('path');
 
 const createfile = async(file, host) => {
 
@@ -13,11 +14,11 @@ const createfile = async(file, host) => {
     let make_string = help.makeString(10);
     var punto_final = sampleFile.name.split('.');
     let type_file = punto_final[punto_final.length - 1];
-    // uploadPath = __dirname + '../../../public/assets/temp_files/' + make_string + "_" + Date.now() + "." + type_file;
     var name = make_string + "_" + Date.now() + "." + type_file
-    uploadPath = __dirname + '../../../public/assets/whatsapp-files/' + name;
+        // uploadPath = __dirname + '/../../public/assets/whatsapp-files/' + name;
+    uploadPath = path.join(__dirname, 'public/assets/whatsapp-files/' + name)
+    console.log(uploadPath);
 
-    // Use the mv() method to place the file somewhere on your server
     let miPrimeraPromise = new Promise((resolve, reject) => {
 
         sampleFile.mv(uploadPath, function(err) {
@@ -26,7 +27,7 @@ const createfile = async(file, host) => {
                 //res.status(500).send(err);
                 resolve({ status: 500 });
             }
-            resolve({ status: 200, url_file: 'https://' + host + '/assets/whatsapp-files/' + name, tipo_file: tipo[0] });
+            resolve({ status: 200, url_file: process.env.PUBLIC + '/assets/whatsapp-files/' + name, tipo_file: tipo[0] });
             // resolve({ status: 200, url_file: 'https://c180f78489a5.ngrok.io/assets/whatsapp-files/' + name, tipo_file: tipo[0] });
             /*'https://www.dzoom.org.es/wp-content/uploads/2017/07/seebensee-2384369-810x540.jpg'*/
             /*'https://c180f78489a5.ngrok.io/assets/whatsapp-files/'*/
